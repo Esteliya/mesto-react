deaa1606f5715054e2f334a6c19a873576ca9a3d
Esteliya <avroradis@gmail.com>
2023-05-26 07:10:27 +0300

    fix: убрали лишний код

--- src/utils/customize.js
+++ src/utils/customize.js
@@ -1,21 +1,3 @@
-const selectors = {
-  formSelector: '.edit-form',//форма
-  inputSelector: '.edit-form__personalia',//инпут в форме
-  buttonSelector: '.popup__save-button',//кнопка сохранения
-  disabledButtonSelector: 'save-button-disabled',//неактивная кнопка
-  inputErrorSelector: 'input-error',//нижнее подчеркивание инпута
-  spanErrorSelector: 'edit-form__personalia-error_active',//активная строка ошибки
-  nameSelector: ".profile__user-firstname",//html-строка имени профиля
-  aboutSelector: ".profile__user-profession",//html-строка профессии
-  avatarSelector: ".profile__avatar",//html <img src=#>
-  templiteSelector: "#templite-card",//блок для карточек
-  popupProfile: ".profile-popup",//попап редактирования профиля
-  popupAddCard: ".add-card-popup",//попап создания новой карточки
-  popupZoom: ".zoom-img-popup",//попап zoom-картинки
-  popupDelete: ".delete-card-popup",//попап подтверждения удаления
-  popupAddAvatar: ".add-avatar-popup",//попап редактирования аватарки
-};
-
 //настройки api
 const apiSetting = {
   url: 'https://mesto.nomoreparties.co/v1/cohort-64',
@@ -27,6 +9,5 @@ const apiSetting = {
 
 //ЭКСПОРТ
 export {
-  selectors,
   apiSetting,
 };
